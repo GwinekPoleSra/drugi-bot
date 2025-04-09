949efa8d4107f36c197963875db123573f2d2fd1
GwinekPoleSra <karmelpolesra@gmail.com>
2025-04-09 18:03:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import requests
 import pickle
+import os
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
